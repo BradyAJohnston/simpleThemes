005c24fa33debf95fb0db757a5e9f197e5f228ae
bradyajohnston <brady.johnston@me.com>
2021-01-18 15:37:01 +1100

    Remove aspect ratio from simpleDark

--- R/simple_dark.R
+++ R/simple_dark.R
@@ -9,7 +9,7 @@
 
 #The plan at some point is co make these levels customisable but that is not the case currently.
 
-simple_dark <- function(AR = 1/2  , linesize = 2){
+simple_dark <- function(linesize = 2){
 
   level1 = "gray20"
   level2 = "gray30"
@@ -22,16 +22,16 @@ simple_dark <- function(AR = 1/2  , linesize = 2){
   theme(panel.background = element_rect(colour = level4, fill = level2, size = linesize),
                         panel.grid = element_line(colour = level3),
                         panel.border = element_rect(fill = NA, colour = level4, size = linesize),
-                        # plot.background = element_rect(fill = level1),
                         plot.background = element_rect(fill = level1, colour = NA),
-                        plot.title = element_text(colour = "white"),
+                        plot.title = element_text(colour = level7),
+                        plot.subtitle = element_text(colour = level6),
+                        plot.caption = element_text(colour = level5),
                         axis.text = element_text(colour = level5),
                         axis.ticks = element_line(colour = level4),
                         axis.title = element_text(colour = level6),
                         # strip.background = element_rect(fill = level2, colour = "gray100", size = 0),
                         strip.background = element_blank(),
                         strip.text = element_text(colour = level7),
-                        aspect.ratio = AR,
                         legend.background = element_blank(),
                         legend.text = element_text(colour = level5),
                         legend.position = "right",
